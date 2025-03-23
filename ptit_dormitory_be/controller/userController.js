28b2252571ba4046f7f07ec0b9b1145b4dc51959
hdang1612 <109531464+Hdang1612@users.noreply.github.com>
2025-03-23 14:46:54 +0700

    feat: CRUD user

--- ptit_dormitory_be/controller/userController.js
+++ ptit_dormitory_be/controller/userController.js
@@ -1,5 +1,52 @@
-import { updateUserService } from '../services/userServices.js';
+import {
+  updateUserService,
+  getListUserService,
+  createUserService,
+  deleteUserService,
+  getUserByIdService,
+} from '../services/userServices.js';
 
+export const getUsersList = async (req, res, next) => {
+  try {
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const search = req.query.search || '';
+    const data = await getListUserService(search, page, limit);
+
+    res.status(200).json({
+      success: true,
+      data: data.users,
+      pagination: data.pagination,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const user = await getUserByIdService(id);
+
+    return res.status(200).json({ success: true, data: user });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Tạo user mới
+export const createUser = async (req, res, next) => {
+  try {
+    const newUser = await createUserService(req.body);
+    res.status(201).json({
+      success: true,
+      message: 'User created successfully',
+      user: newUser,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 export const updateUser = async (req, res, next) => {
   try {
     const userId = req.params.id;
@@ -11,13 +58,25 @@ export const updateUser = async (req, res, next) => {
       return res.status(404).json({ message: result.message });
     }
 
-    res
-      .status(200)
-      .json({
-        success: true,
-        message: 'User updated successfully',
-        user: result.user,
-      });
+    res.status(200).json({
+      success: true,
+      message: 'User updated successfully',
+      user: result.user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await deleteUserService(id);
+
+    res.status(200).json({
+      success: true,
+      message: result.message,
+    });
   } catch (error) {
     next(error);
   }
