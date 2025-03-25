518e0e971cb9517d4c40781e94e935dfbeadfe17
bachxuantrann <bxt203@gmail.com>
2025-03-25 14:13:35 +0700

    write new api crud place

--- ptit_dormitory_be/routes/areaRoute.js
+++ ptit_dormitory_be/routes/areaRoute.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import areaController from '../controller/areaController.js';
 const areaRoutes = express.Router();
+areaRoutes.get('/getPlaces', areaController.getPlaces);
+areaRoutes.get('/getPlaceDetail/:id', areaController.getPlaceDetail);
+areaRoutes.post('/createPlaces', areaController.createPlaces);
+areaRoutes.put('/updatePlaces/:id', areaController.updatePlaces);
 
+// Test
 areaRoutes.get('/area', areaController.getAreas);
 areaRoutes.post('/area', areaController.addNewArea);
 areaRoutes.put('/area/:id', areaController.updateArea);
@@ -13,4 +18,5 @@ areaRoutes.get('/room/getRoomsByArea', areaController.getRoomsByArea);
 areaRoutes.get('/room', areaController.getAndFindRooms);
 areaRoutes.post('/room', areaController.addNewRoom);
 areaRoutes.put('/room/:id', areaController.updateRoom);
+
 export default areaRoutes;
