7ddd6e05067ecf077aed6eed86cf4ba8a0fb83bd
hdang1612 <109531464+Hdang1612@users.noreply.github.com>
2025-03-30 16:24:50 +0700

    feat: role cru

--- ptit_dormitory_be/routes/Routes.js
+++ ptit_dormitory_be/routes/Routes.js
@@ -4,12 +4,14 @@ import { verifyToken, authorizeRoles } from '../middleware/auth.js';
 import authRoutes from './authRoutes.js';
 import userRoutes from './userRoutes.js';
 import areaRoutes from './areaRoute.js';
+import roleRoutes from './roleRoutes.js';
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/user', userRoutes);
 router.use('/place', areaRoutes);
+router.use('/role', roleRoutes);
 
 
 export default router;
