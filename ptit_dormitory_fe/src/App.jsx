ab927f2c807fa8cdfcf6d204e0d372a280441e11
hoanganh2212az <88610488+hoanganh2212az@users.noreply.github.com>
2025-04-13 01:05:16 +0700

    Add files via upload

--- ptit_dormitory_fe/src/App.jsx
+++ ptit_dormitory_fe/src/App.jsx
@@ -1,10 +1,17 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Frame } from "./view/Frame";
+import { PowerMonitoring } from "./view/PowerMonitoring";
+import { Reports } from "./view/Reports";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <div className="w-[20px] bg-[#000] h-screen ">hello</div>
-    </>
+    <Routes>
+      <Route path="/" element={<Frame />} />
+      <Route path="/power-monitoring" element={<PowerMonitoring />} />
+      <Route path="/reports" element={<Reports />} />
+    </Routes>
   );
 }
 
