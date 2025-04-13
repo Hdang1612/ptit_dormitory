fb86e3886790bc9db06db7c44e2c131dbb1d7435
bachxuantrann <bxt203@gmail.com>
2025-04-13 10:46:10 +0700

    create model shift_schedule,report,attendance

---
+++ ptit_dormitory_be/models/Report.js
@@ -0,0 +1,39 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../config/db.js';
+
+const Report = sequelize.define(
+  'Report',
+  {
+    id: {
+      type: DataTypes.STRING(255),
+      primaryKey: true,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT('long'),
+      allowNull: true,
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    create_by: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+    },
+    checkin_photo: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+    },
+    attendance_id: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+      unique: true,
+    },
+  },
+  {
+    tableName: 'reports',
+    timestamps: false,
+  },
+);
+export default Report;
